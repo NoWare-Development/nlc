818f4bdf76e123cde589a0bc9cf905fc83f22b7b
Nyvyme <mixagame228@gmail.com>
2025-08-15 02:17:32 +0500

    parser: Support for arguments with modifiers in function prototypes.
    Resolves #13

--- libnlc/parser/funcarg.cpp
+++ libnlc/parser/funcarg.cpp
@@ -1,3 +1,4 @@
+#include "libnlc/parser/macros.hpp"
 #include "parser.hpp"
 
 namespace nlc
@@ -9,17 +10,22 @@ Parser::parse_function_argument ()
   auto cur = _tokens.at (_pos);
   if (cur.type == TokenType::TOKEN_PERIOD)
     {
-      TokenType next_two[2] = {
-        peek (_pos + 1),
-        peek (_pos + 2),
-      };
+      VERIFY_POS (_pos + 1);
+      VERIFY_POS (_pos + 2);
+      VERIFY_TOKEN (_pos + 1, peek (_pos + 1), TokenType::TOKEN_PERIOD);
+      VERIFY_TOKEN (_pos + 2, peek (_pos + 2), TokenType::TOKEN_PERIOD);
 
-      if (next_two[0] == TokenType::TOKEN_PERIOD && next_two[0] == next_two[1])
-        {
-          AST out (_pos, ASTType::AST_VARIADIC);
-          _pos += 3;
-          return out;
-        }
+      AST out (_pos, ASTType::AST_VARIADIC);
+      _pos += 3;
+      return out;
+    }
+
+  if (cur.type == TokenType::TOKEN_ID && is_modifier (cur.value))
+    {
+      AST modifier_arg (_pos++, ASTType::AST_MODIFIER, cur.value);
+      auto argument = parse_function_argument ();
+      modifier_arg.append (argument);
+      return modifier_arg;
     }
 
   return parse_variable_decldef ();
